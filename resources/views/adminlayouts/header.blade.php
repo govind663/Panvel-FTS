8dee670ab4aa98790c3ebccab133f2c6eddc08b5
govind66301~ <whitdevil06@gmail.com>
2024-01-25 18:19:01 +0530

    PMC_FTS_Version - 0.4

--- resources/views/adminlayouts/header.blade.php
+++ resources/views/adminlayouts/header.blade.php
@@ -446,15 +446,11 @@
     <script src="{{ url('/') }}/assets/vendors/scripts/script.min.js"></script>
     <script src="{{ url('/') }}/assets/vendors/scripts/process.js"></script>
     <script src="{{ url('/') }}/assets/vendors/scripts/layout-settings.js"></script>
-    <script src="{{ url('/') }}/assets/src/plugins/apexcharts/apexcharts.min.js"></script>
 
     <script src="{{ url('/') }}/assets/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
     <script src="{{ url('/') }}/assets/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
     <script src="{{ url('/') }}/assets/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
     <script src="{{ url('/') }}/assets/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
-
-    <script src="{{ url('/') }}/assets/vendors/scripts/dashboard3.js"></script>
-
 
     <!-- buttons for Export datatable -->
     <script src="{{ url('/') }}/assets/src/plugins/datatables/js/dataTables.buttons.min.js"></script>
